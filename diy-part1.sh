8e30c63744b4d28ba9369194b3d8280d27775ab4
Ljzkirito <Kirito.ljz@outlook.com>
2021-04-16 21:28:42 +0800

    upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,38 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+
+#关机（增加关机功能）
+svn co https://github.com/Ljzkirito/openwrt-packages/trunk/luci-app-poweroff package/lean/luci-app-poweroff
+# 获取luci-app-passwall以及缺失的依赖
+pushd package/lean
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng
+popd
+# 使用官方ppp 2.4.9
+rm -rf package/network/services/ppp
+svn co https://github.com/Ljzkirito/openwrt-packages/trunk/ppp package/network/services/ppp
+#svn co https://github.com/openwrt/openwrt/trunk/package/network/services/ppp package/network/services/ppp
+# Remove upx commands
+sed -i "/upx/d" package/lean/UnblockNeteaseMusicGo/Makefile || true
+sed -i "/upx/d" package/lean/frp/Makefile || true
+sed -i "/upx/d" package/lean/v2ray-plugin/Makefile || true
+# Add luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
+#Remove default apps
+sed -i 's/luci-app-vsftpd //g' include/target.mk
+sed -i 's/luci-app-unblockmusic //g' include/target.mk
+sed -i 's/luci-app-vlmcsd //g' include/target.mk
+sed -i 's/luci-app-nlbwmon //g' include/target.mk
+sed -i 's/luci-app-accesscontrol //g' include/target.mk
+#关闭 禁止解析 IPv6 DNS 记录
+sed -i 's/option filter_aaaa\t1/option filter_aaaa\t0/g' package/network/services/dnsmasq/files/dhcp.conf
+#Remove firewall zone wan6
+sed -i "/wan6/d" package/network/config/firewall/files/firewall.config
